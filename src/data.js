0dc9cd30cc568d89a27d51ca0e9253b481f66eff
annleit <annlei50639@stud.noroff.no>
2025-02-23 23:52:33 +0100

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,31 +1,215 @@
-let shopItemsData = [
-    {
-       id:"etfjdfigfh",
-       name:"Jacket nr5",
-       price: 45,
-       desc:"lorem ipsum",
-       img:"img/img/3-habita-jacket.jpg",
-    },
-    {
-       id:"etfjdfigfh",
-       name:"Jacket nr5",
-       price: 45,
-       desc:"lorem ipsum",
-       img:"img/img/3-habita-jacket.jpg",
-    },
-    {
-       id:"etfjdfigfh",
-       name:"Jacket nr5",
-       price: 45,
-       desc:"lorem ipsum",
-       img:"img/img/3-habita-jacket.jpg",
-    },
-    {
-       id:"etfjdfigfh",
-       name:"Jacket nr5",
-       price: 45,
-       desc:"lorem ipsum",
-       img:"img/img/3-habita-jacket.jpg",
-    },
+let shopItemsData = {"data":
+
+   [
+   {
+   "id":"07a7655a-7927-421b-ba6a-b6742d5a75b8",
+   "title":"Rainy Days Thunderbolt Jacket",
+   "description":"The Women's Rainy Days Thunderbolt jacket is a sleek and stylish waterproof jacket perfect for any outdoor adventure.",
+   "gender":"Female",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Black",
+   "price":139.99,
+   "discountedPrice":139.99,
+   "onSale":false,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/9-thunderbolt-jacket.jpg",
+   "alt":"A brown jacket with a blue patch on the chest"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   },
    
-   ];
+   {
+   "id":"298d6c5f-5445-4581-9ff5-be921f4ce37c",
+   "title":"Rainy Days Habita Jacket",
+   "description":"The Men's Rainy Days Habita jacket is a relaxed fit with breathable material that is a packable answer to uncertain weather.",
+   "gender":"Male",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Black",
+   "price":129.99,
+   "discountedPrice":129.99,
+   "onSale":false,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/3-habita-jacket.jpg",
+   "alt":"A black and white jacket with yellow accents"},
+   "tags":["jacket","mens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"6e5ae9e6-2033-4c63-82b9-5b58226425f4",
+   "title":"Rainy Days VitaForce Jacket",
+   "description":"The Women's Rainy Days VitaForce jacket is a breathable and sustainable waterproof jacket for hiking.",
+   "gender":"Female",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Green",
+   "price":124.99,"discountedPrice":124.99,
+   "onSale":false,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/6-vitaforce-jacket.jpg","alt":"A brown jacket with a zipper on the front"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"97e77845-a485-4301-827f-51b673d4230f",
+   "title":"Rainy Days M83 Jacket",
+   "description":"The Women's Rainy Days M83 jacket delivers waterproof, breathable protection from head to waist. Perfect for the adventure seekers.",
+   "gender":"Female",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Black",
+   "price":109.99,
+   "discountedPrice":99.99,
+   "onSale":true,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/1-m83-jacket.jpg",
+   "alt":"A black jacket with a white logo on it"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"987cabbf-b41e-4353-8bf2-3fbfbbf0e7d5",
+   "title":"Rainy Days Silverbreeze Jacket",
+   "description":"The Men's Rainy Days Silverbreeze jacket is the ultimate waterproof rain jacket for those stormy weather conditions.",
+   "gender":"Male",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Gray",
+   "price":139.99,
+   "discountedPrice":114.99,
+   "onSale":true,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/4-silverbreeze-jacket.jpg",
+   "alt":"A grey jacket with a zipper on the front"}
+   ,"tags":["jacket","mens"],
+   "favorite":true
+   },
+   
+   {
+   "id":"b01f4f5f-7cb6-489a-b58e-849db1d40187",
+   "title":"Rainy Days Rock Creek Jacket",
+   "description":"The Men's Rainy Days Rock Creek jacket is a stylish and comfortable rain jacket perfect for city wear.",
+   "gender":"Male",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Blue",
+   "price":149.99,"discountedPrice":139.99,
+   "onSale":true,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/7-rock-creek-jacket.jpg",
+   "alt":"A red and black jacket on a white background"},
+   "tags":["jacket","mens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"b4eaa52e-2efe-4075-9772-e0c6d7ba04bb",
+   "title":"Rainy Days Venture Jacket",
+   "description":"The Women's Rainy Days Venture jacket is a lightweight and packable rain jacket that is perfect for travel.",
+   "gender":"Female",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Purple",
+   "price":99.99,
+   "discountedPrice":89.99,
+   "onSale":true,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/10-venture-jacket.jpg",
+   "alt":"A navy fleece jacket with a zipper on the front"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   ,
+   
+   {
+   "id":"b8b528fc-6c60-41f6-a5a9-9a8b27a9482a",
+   "title":"Rainy Days Akra Jacket",
+   "description":"The Men's Rainy Days Akra jacket is bound to be your new go-to water-repellent rain jacket.",
+   "gender":"Male",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Red",
+   "price":129.99,
+   "discountedPrice":119.99,
+   "onSale":true,"image":{"url":"https://static.noroff.dev/api/rainy-days/0-akra-jacket.jpg",
+   "alt":"An orange and grey jacket with a hood"},
+   "tags":["jacket","mens"],
+   "favorite":true
+   },
+   
+   {
+   "id":"c54dc861-c62b-4fe7-9a12-17868c25b847",
+   "title":"Rainy Days Gelventure Jacket",
+   "description":"The Men's Rainy Days Gelventure jacket is the easy answer to all your commuting needs, whether in the city or forest.",
+   "gender":"Male","sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Red","price":179.99,"discountedPrice":169.99,
+   "onSale":true,"image":{"url":"https://static.noroff.dev/api/rainy-days/2-gelventure-jacket.jpg",
+   "alt":"A red and black jacket on a white background"},
+   "tags":["jacket","mens"],
+   "favorite":true
+   },
+   
+   {
+   "id":"d2e44336-7c12-4a41-bf06-133399da5bd9",
+   "title":"Rainy Days Puddle Jumper Jacket",
+   "description":"The Women's Rainy Days Puddle Jumper jacket is a stylish and practical jacket for those rainy days.",
+   "gender":"Female",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Yellow",
+   "price":149.99,
+   "discountedPrice":139.99,
+   "onSale":true,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/11-puddle-jumper-jacket.jpg",
+   "alt":"A women's jacket with a hood and zippers"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"f396ebf1-357d-4181-b061-9a0468792efe",
+   "title":"Rainy Days TrailBlaze Jacket",
+   "description":"The Women's Rainy Days TrailBlaze jacket is a durable and waterproof jacket that is perfect for hiking and other outdoor activities.",
+   "gender":"Female","sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Green",
+   "price":179.99,"discountedPrice":179.99,
+   "onSale":false,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/8-trailblaze-jacket.jpg",
+   "alt":"A blue jacket with a hood on it"},
+   "tags":["jacket","womens"],
+   "favorite":false
+   },
+   
+   {
+   "id":"ff94a6eb-524b-4a56-b326-92fd13ee0918",
+   "title":"Rainy Days XX Jacket",
+   "description":"The Men's Rainy Days XX jacket is a lightweight, packable jacket for those unexpected adventures.",
+   "gender":"Male",
+   "sizes":["XS","S","M","L","XL","XXL"],
+   "baseColor":"Black",
+   "price":109.99,
+   "discountedPrice":109.99,
+   "onSale":false,
+   "image":{"url":"https://static.noroff.dev/api/rainy-days/5-xx-jacket.jpg",
+   "alt":"A black and grey jacket with white accents"},
+   "tags":["jacket","mens"],
+   "favorite":true
+   }
+   ],
+   
+   "meta":{"isFirstPage":true,"isLastPage":true,"currentPage":1,"previousPage":null,"nextPage":null,"pageCount":1,"totalCount":12}}
+   
+   
+   
+   
+   
+   https://static.noroff.dev/api/rainy-days/9-thunderbolt-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/3-habita-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/6-vitaforce-jacket.jpg
+   
+   https://static.cloud.noroff.dev/api/rainy-days/1-m83-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/4-silverbreeze-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/7-rock-creek-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/10-venture-jacket.jpg
+   
+   https://static.cloud.noroff.dev/api/rainy-days/0-akra-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/2-gelventure-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/11-puddle-jumper-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/8-trailblaze-jacket.jpg
+   
+   https://static.noroff.dev/api/rainy-days/5-xx-jacket.jpg
